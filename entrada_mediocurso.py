2e9d26a316bfe578578e69e7a10f7d877d8e72fc
Sergiohdz <sergiohdz92@gmail.com>
2012-11-19 21:14:31 -0600

    Entrada Color

--- entrada_mediocurso.py
+++ entrada_mediocurso.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
  
-#Importamos las librerias necesarias
 import Image, sys
 
-#Creamos un nuevo archivo para guardas los datos binarios 
-f = open("huella1.txt", "w")
+f = open("ImagenBinaria.txt", "w") #Se crea archivo para guardar la imagen en binario
+g = open("ImagenColores.txt", "w") #Se crea archivo para guardar los colores en RGB de cada pixel de la imagen
 
-#Variables
 x = 1
 y = 1
+lista = []
+sum = 0
+im = Image.open("img8.png") 
+Ancho=178
+Largo=140
 
-#Abrimos la huella que ocupamos
-im = Image.open("img1.png") 
 
-#Hacemos un recorrido en largo y ancho de la imagen por pixeles
-for y in range(0,209): #Largo
-	for x in range(0,180): #Ancho
-		pix = im.load()             
+for y in range(0,Ancho): #Ancho
+	for x in range(0,Largo): #Largo
+		pix = im.load()
 		pix[x, y]
-                if pix[x,y] == (255,255,255):  #Si se encuentra un punto blanco se guarda 1
+                if pix[x,y] == (255,255,255): #Color Blanco en RGB
 	       	        c= 1
 		else:
-			c= 0                   #Si no es blanco entonces se guarda 0
-		f.write(str(c))            #Escribimos en el archivo
+			c= 0
+		f.write(str(c))
 	f.write('\n')
-f.close()					       #Cerramos el archivo
 
-print "Huella grabada con exito"
-
+var = im.getcolors()
+g.write(str(var))
