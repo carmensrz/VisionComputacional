42a6a3e0ee8fe0b9c40fb6dc73e340660253c674
Sergiohdz <sergiohdz92@gmail.com>
2012-11-20 02:27:06 -0600

    IdentificacionFase3

--- huellas_ordinario_identificacion.py
+++ huellas_ordinario_identificacion.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import glob
+Arc = glob.glob("*.txt")
 
-directory=os.path.join("/home/daniel/Desktop/proyecto")
+directory=os.path.join("C:\Redes")
+
+c1 = open("Desconocido.txt", "r")
+archivodesconocido = c1.readlines()
 
 for root,dirs,files in os.walk(directory):
 	for file in files:
-		if file.endswith(".txt"):           
-			print "encontrado"
+		if file.endswith(".txt"): 
+			c2 = open(file, "r")
+			archivoencontrado = c2.readlines()
+			if archivodesconocido == archivoencontrado:
+				print "Es igual"
+			else:
+				print "No es igual"			
 
+
+			
 
